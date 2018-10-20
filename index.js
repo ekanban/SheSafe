adf1d993d4549a045475db5aaa4e753390390436
Ace-Bansal <bansalekansh@gmail.com>
2018-10-20 18:00:38 +0530

    Add complaint api and mail

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var User = require("./models/users.js");
+var Complaint = require("./models/complaints.js");
+
 var expressSession = require("express-session");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
 var passportLocalMongoose = require("passport-local-mongoose");
 var mongoose = require("mongoose");
+var nodemailer = require('nodemailer');
+
+// mongoose.connect("mongodb://avadacodavra:shesafe10@ds125502.mlab.com:25502/shesafe",
+//   { useNewUrlParser: true }
+//   )
+//   .then(() => {
+//     console.log("Connected to DB");
+//   });
+
 mongoose.connect("mongodb://localhost/shesafe");
 
 app.use(expressSession({
@@ -73,6 +84,47 @@ app.get("/logout", function(req, res){
   req.logout();
   res.redirect("/");
 });
+
+//==============================================================================
+//======================   COMPLAINT ROUTES   ==================================
+//==============================================================================
+app.post("/complaint", function(req, res){
+  var payload = req.body.complaint;
+  Complaint.create(payload, function(err, createdComplaint){
+    if(err){
+      console.log("Error at the complaint route.");
+    } else {
+      console.log(createdComplaint);
+
+      //==========================SEND MAIL=====================================
+
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'switchbooks101@gmail.com',
+          pass: 'switch10books@!'
+        }
+      });
+
+      var mailOptions = {
+        from: 'switchbooks101@gmail.com',
+        to: 'iec2017016@iiita.ac.in',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+      res.redirect("/");
+    }
+  })
+})
 
 
 
